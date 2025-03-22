169d748d27c646b42fb0ca7ae6f30f9c9a85cbb7
skcreator 7 <118694333+skcreator7@users.noreply.github.com>
2025-03-22 23:00:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,55 +16,50 @@ Bot = Client(
 )
 
 START_TEXT = """Hello {},
-I am a simple youtube thumbnail downloader telegram bot.
+I am a simple YouTube thumbnail downloader Telegram bot.
 
-- Send a youtube video link or video ID.
+- Send a YouTube video link or video ID.
 - I will send the thumbnail.
-- You can also send youtube video link or video id with quality. ( like :- `rokGy0huYEA | sd`
+- You can also send a YouTube video link or video ID with quality. (like: `rokGy0huYEA | sd`)
   - sd - Standard Quality
   - mq - Medium Quality
   - hq - High Quality
   - maxres - Maximum Resolution
 """
 
-BUTTON = [InlineKeyboardButton("Movies Search Group", url='https://t.me/+_AWkWy0499dlZjQ1')]
+START_BUTTONS = InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton("🎬 Wʜᴀᴛꜱᴀᴘᴘ Mᴏᴠɪᴇꜱ Cʜᴀɴɴᴇʟ", url='https://whatsapp.com/channel/0029VaCUrJwEAKW5rDJXz23h')],
+        [InlineKeyboardButton("🎬 Wʜᴀᴛꜱᴀᴘᴘ Mᴏᴠɪᴇꜱ Cʜᴀɴɴᴇʟ 2", url='https://whatsapp.com/channel/0029Va69Ts2C6ZvmEWsHNo3c')],
+        [InlineKeyboardButton("🔎 Mᴏᴠɪᴇꜱ Sᴇᴀʀᴄʜ Gʀᴏᴜᴘ", url='https://t.me/+_AWkWy0499dlZjQ1')],
+        [InlineKeyboardButton("🔎 Mᴏᴠɪᴇꜱ Sᴇᴀʀᴄʜ Gʀᴏᴜᴘ 2", url='https://t.me/+cxiYHGE4jW9jOTY9')],
+        [InlineKeyboardButton("📢 Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ", url='https://t.me/SkFilmbox')],
+        [InlineKeyboardButton("☎️ Aᴅᴍɪɴ", url='https://t.me/Skadminrobot')]
+    ]
+)
 
 @Bot.on_callback_query()
-async def cb_data(_, message):
-    data = message.data.lower()
-    if data == "qualities":
-        await message.answer('Select a quality')
-        buttons = []
-        for quality in ytthumb.qualities():
-            buttons.append(
-                InlineKeyboardButton(
-                    text=ytthumb.qualities()[quality],
-                    callback_data=quality
-                )
-            )
-    if data == "back":
-        await message.edit_message_reply_markup(photo_buttons)
+async def cb_data(_, callback_query):
+    data = callback_query.data.lower()
     if data in ytthumb.qualities():
         thumbnail = ytthumb.thumbnail(
-            video=message.message.reply_to_message.text,
-            quality=message.data
+            video=callback_query.message.reply_to_message.text,
+            quality=data
         )
-        await message.answer('Updating')
-        await message.edit_message_media(
-            media=InputMediaPhoto(media=thumbnail),
-            reply_markup=photo_buttons
+        await callback_query.answer('Updating')
+        await callback_query.edit_message_media(
+            media=InputMediaPhoto(media=thumbnail)
         )
-        await message.answer('Updated Successfully')
-
+        await callback_query.answer('Updated Successfully')
 
 @Bot.on_message(filters.private & filters.command(["start", "help"]))
 async def start(_, message):
     await message.reply_text(
         text=START_TEXT.format(message.from_user.mention),
         disable_web_page_preview=True,
+        reply_markup=START_BUTTONS,
         quote=True
     )
-
 
 @Bot.on_message(filters.private & filters.text)
 async def send_thumbnail(bot, update):
@@ -75,8 +70,8 @@ async def send_thumbnail(bot, update):
     )
     try:
         if " | " in update.text:
-            video = update.text.split(" | ", -1)[0]
-            quality = update.text.split(" | ", -1)[1]
+            video = update.text.split(" | ")[0]
+            quality = update.text.split(" | ")[1]
         else:
             video = update.text
             quality = "sd"
@@ -91,10 +86,9 @@ async def send_thumbnail(bot, update):
         await message.delete()
     except Exception as error:
         await message.edit_text(
-            text="Please join movies search Group and get movies",
+            text="Please join the WhatsApp & Movies Search Group and get movies",
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([BUTTON])
+            reply_markup=START_BUTTONS
         )
-
 
 Bot.run()
